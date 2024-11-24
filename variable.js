28fd515866a134c0ce46f0afe62fcc7d642a9a9e
Shivachaitanya3580 <shivachiathu003@gmail.com>
2024-11-24 22:31:46 +0530

    var and datatypes are declared

---
+++ variable.js
@@ -0,0 +1,36 @@
+var a=10;
+console.log(a);
+
+let b=20;
+ b=30;
+console.log(b);
+
+const c=30;
+console.log(c);
+{
+    let d=40;
+    console.log(d);
+    
+}
+{
+    const e=50;
+    console.log(e);
+    
+}
+ 
+var num;
+num=60;
+num=70;
+console.log(num);
+
+let fun;
+fun = 20;
+fun = 30;
+console.log(fun)
+let i=30;
+i=20;
+console.log(i)
+
+const pi=3.14;
+pi=2.14;
+console.log(pi)
